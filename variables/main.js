7c0cdd8d63ce5e5c112f70d029d15827ebda10c2
Svift <kuryakov-s@mail.ru>
2019-04-18 21:57:37 +0700

    Выполнил задания

---
+++ variables/main.js
@@ -0,0 +1,28 @@
+// Задание 1
+
+let algebra = 4,
+    geography = 5,
+    physics = 5,
+    averageRating;
+
+averageRating = (algebra + geography + physics)/3;
+
+console.log(averageRating);
+
+// Задание 2
+
+let name = 'Сергей',
+    message = `Привет, мир! Меня зовут ${name}`;
+
+console.log(message);
+
+// Задание 3
+
+let x = 2,
+    y = 22,
+    z = 0,
+    result;
+
+result = x * y + 5 * z + x - 1;
+
+console.log(result);
