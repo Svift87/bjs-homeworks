a4d1b76c969b518a47e6d2912d9ffd67286262bb
Svift <kuryakov-s@mail.ru>
2019-05-17 23:30:17 +0700

    array-proto-hocs

---
+++ array-proto-hocs/main.js
@@ -0,0 +1,61 @@
+function compareArrays( arr1, arr2 ) {
+    if (arr1.length != arr2.length) {
+        console.log(`Массивы ${arr1} и ${arr2} различаются`);
+        return false;
+    }
+    for (let i = 0; arr1.length > i; i++) {
+        if (arr1[i] == arr2[i]) {
+            console.log(`Массивы ${arr1} и ${arr2} идентичны`);
+            return true;
+            
+        } else {
+            console.log(`Массивы ${arr1} и ${arr2} различаются`);
+            return false;
+        }    
+    }
+}
+
+compareArrays([8, 9], [6]);
+compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]);
+compareArrays([9, 2, 4, 8, 2], [9, 2, 4]);
+compareArrays([1, 2, 3], [2, 3, 1]);
+compareArrays([8, 1, 2], [8, 1, 2]);
+
+function memoize(fn, limit) {
+    let results = [];
+
+    return function(...rest) {
+		let args = [...rest];        
+		let a = results.find(e1 => compareArrays(e1.args, args));
+
+		if (a) {
+			return a.result;
+		} else {
+			results.unshift({
+				args,
+				result: fn(...rest)
+			})
+
+			if (results.length > limit) {
+				results.shift();
+			}
+
+			return results;
+		}
+	}
+}
+
+let sum = function(...rest) {
+	let sum = 0;
+	for (let i = 0; i < rest.length; i++) {
+		sum += rest[i];
+	}
+	return (sum);
+}
+
+let mSum = memoize(sum, 10);
+
+mSum(3, 4, 5, 6);
+mSum(3, 4, 5, 6);
+mSum(3, 4, 5);
+mSum(3, 4, 6);
